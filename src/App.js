587f3b437a79a74cd4db25972750debcfcfe9e12
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-04-18 15:10:21 -0700

    added add and close toggle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -25,6 +26,13 @@ const App = () => {
     },
   ]);
 
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1;
+    const newTask = { id, ...task };
+    setTasks([...tasks, newTask]);
+  };
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id));
@@ -41,8 +49,11 @@ const App = () => {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask />
+      <Header
+        onAdd={() => setShowAddTask(!showAddTask)}
+        showAdd={showAddTask}
+      />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,30 +1,30 @@
-import PropTypes from 'prop-types';
-import Button from './Button';
+import PropTypes from "prop-types";
+import Button from "./Button";
 
-const Header = ({ title }) => {
-  const onClick = () => {
-    console.log('Click')
-  }
-  
+const Header = ({ title, onAdd, showAdd }) => {
   return (
-    <header className='header'>
-        <h1>{title}</h1>
-        <Button color='green' text='Add' onClick={onClick} />
+    <header className="header">
+      <h1>{title}</h1>
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
-  )
-}
+  );
+};
 
 Header.defaultProps = {
-    title: 'React Task Tracker',
-}
+  title: "React Task Tracker",
+};
 
 Header.propTypes = {
-    title: PropTypes.string.isRequired,
-}
+  title: PropTypes.string.isRequired,
+};
 // CSS IN JS //
 // const headingStyle = {
 //   color: 'red',
 //   backgroundColor: 'black',
 // }
 
-export default Header
+export default Header;
