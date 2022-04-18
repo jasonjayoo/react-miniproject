dac41c3ad67101245e9444b49ee60df463d5f9b0
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-04-18 14:53:35 -0700

    added form inputs for tasks, date and time, and checkbox

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -22,20 +23,33 @@ const App = () => {
       day: "May 2nd at 10:30pm",
       reminder: false,
     },
-  ])
+  ]);
 
   // Delete Task
   const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id))
-  }
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <Header />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} />) : ('No Tasks Available')}
+      <AddTask />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks Available"
+      )}
     </div>
-
   );
-}
+};
 
 export default App;
